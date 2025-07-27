35aa46bfec1751089e8255fb766b6a217665ab29
willevs1 <wills.msn@hotmail.co.uk>
2025-07-27 15:06:49 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,6 +2,17 @@ import streamlit as st
 
 st.title("Music ML Model")
 
+# Download latest version
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("https://raw.githubusercontent.com/willevs1/MusicML/main/Spotify_2024_Global_Streaming_Data.csv")
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 
 
