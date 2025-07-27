5cfa139b4d59e193b1e27c46367b2c51498c290a
willevs1 <wills.msn@hotmail.co.uk>
2025-07-27 15:06:09 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,19 +1,7 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="ESG Real Estate Model",
-    page_icon="🌱",
-    layout="centered"
-)
+st.title("Music ML Model")
 
-st.title("🌱 ESG Real Estate Modelling Tool")
 
-st.markdown("""
-Welcome to the ESG Real Estate multipage app.
-
-Use the sidebar to switch between:
-- 🏠 **Home**
-- 📊 **Analysis**
-""")
 
 
